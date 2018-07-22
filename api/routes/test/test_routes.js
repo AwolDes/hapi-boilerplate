8f543d0af57fdfcc0ffab2aef12f632edd8efcbf
Lachlan <lachlan_young@hotmail.com>
2018-07-22 14:52:07 +1000

    up to date

--- api/routes/test/test_routes.js
+++ api/routes/test/test_routes.js
@@ -2,58 +2,53 @@ import Knex from "../../knex";
 import server from "../../server";
 
 let test_routes = [
-  {
+{
     path: "/",
     method: "GET",
     handler: (request, reply) => {
-      reply({
-        hello: "world"
-      })
+        return 'Hello World'
     }
-  },
-  {
+},
+{
     path: "/hello/{user_id}",
     method: "GET",
-    handler: (request, reply) => {
+    handler: (request) => {
+        const { user_id } = request.params;
+        return { hello: user_id }
 
-      const { user_id } = request.params;
-      reply({
-        hello: user_id
-      })
-
-      // Knex handles SQL queries with your DB like this!
-      // In general, the Knex operation is like Knex("TABLE_NAME").where(...).chainable(...).then(...)
-      // const getOperation = Knex("Waypoint").where({
-      //
-      //   user_id: user_id
-      //
-      // }).select().then((results) => {
-      //
-      //   if (!results || results.length === 0) {
-      //
-      //     reply({
-      //
-      //       error: true,
-      //       errMessage: "no route found",
-      //
-      //     });
-      //
-      //   }
-      //
-      //   reply({
-      //
-      //     data_length: results.length,
-      //     data: results,
-      //
-      //   });
-      //
-      // }).catch((err) => {
-      //
-      //   reply(err);
-      //
-      // });
+        // Knex handles SQL queries with your DB like this!
+  // In general, the Knex operation is like Knex("TABLE_NAME").where(...).chainable(...).then(...)
+  // const getOperation = Knex("Waypoint").where({
+  //
+  //   user_id: user_id
+  //
+  // }).select().then((results) => {
+  //
+  //   if (!results || results.length === 0) {
+  //
+  //     reply({
+  //
+  //       error: true,
+  //       errMessage: "no route found",
+  //
+  //     });
+  //
+  //   }
+  //
+  //   reply({
+  //
+  //     data_length: results.length,
+  //     data: results,
+  //
+  //   });
+  //
+  // }).catch((err) => {
+  //
+  //   reply(err);
+  //
+  // });
     }
-  }
+}
 ];
 
 export default test_routes;
--- api/server.js
+++ api/server.js
@@ -1,41 +1,37 @@
 import Hapi from "hapi";
 import test_routes from "./routes/test/test_routes";
 
-export const server = new Hapi.Server();
-
-server.connection( {
-  port: 8080
-});
+export const server = new Hapi.Server({ port: 3000, host: 'localhost' });
 
 // For auth
-server.register( require( "hapi-auth-jwt" ), ( err ) => {
-  server.auth.strategy( "token", "jwt", {
-
-  key: "vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy",
-
-  verifyOptions: {
-    algorithms: [ "HS256" ],
-  }
-
-} );
-
-} );
+// server.register( require( "hapi-auth-jwt" ), ( err ) => {
+//   server.auth.strategy( "token", "jwt", {
+//
+//   key: "vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy",
+//
+//   verifyOptions: {
+//     algorithms: [ "HS256" ],
+//   }
+//
+// } );
+//
+// } );
 
 // Lists multiple endpoints from a module in ./routes
 for (let route in test_routes) {
   server.route(test_routes[route]);
 }
 
-server.start( err => {
+async function start() {
+    try {
+        await server.start();
+    }
+    catch (err) {
+        // Fancy error handling here
+        console.error( "Error was handled!" );
+        console.error( err );
+    }
+    console.log( `Server started at ${ server.info.uri }` );
+}
 
-  if( err ) {
-
-    // Fancy error handling here
-    console.error( "Error was handled!" );
-    console.error( err );
-
-  }
-
-  console.log( `Server started at ${ server.info.uri }` );
-
-} );
+start();
